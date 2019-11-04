5fe64655f2cb77d76368f73c41c99f41565bee50
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Pro.local>
2019-11-03 17:50:41 -0800

    updated resume and email

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -12,5 +12,6 @@
   }
 </style>
 <section class="contact d-flex justify-content-between align-items-center px-5 py-4">
-  <a class="contact__cta" href="mailto:lucashugdahl@hotmail.com">lucashugdahl@hotmail.com</a>
+  <a class="contact__cta" href="mailto:lucashugdahl@hotmail.com">lucashugdahl@gmail.com</a>
+  <a class="contact__cta" href="/files/resume.pdf" target="_blank">Resume</a>
 </section>
